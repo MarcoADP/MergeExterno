21da9135c0e9c0a020a73fd268f102bd2016fb8b
Marco Aurélio Paulino <marco23_aurelio@hotmail.com>
2015-09-08 12:58:40 -0300

    comeco
    #comeco

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int tamBufferEntrada;
+int tamBufferSaida;
+
+
+recebeParametros(){
+	
+}
+
+int main(int argc, char const *argv[]) {
+	void recebeParametros();
+    return 0;
+}
